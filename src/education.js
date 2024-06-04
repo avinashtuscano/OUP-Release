896f88837fb9976b4817e0025df4b538ae5436fe
Avinash Tuscano <avinashtuscano@gmail.com>
2024-06-04 09:35:19 +0100

    consolidated fetch. thanks to alex iuga

--- src/education.js
+++ src/education.js
@@ -16,23 +16,32 @@ const init = async () => {
 
 const fetchAllData = async () => {
   let finalData = {};
-  try {
-    const devData = await fetchData(DEV_URL);
-    const testData = await fetchData(TEST_URL);
-    const perfData = await fetchData(PERF_URL);
-    const uatData = await fetchData(UAT_URL);
-    const preProdData = await fetchData(PPROD_URL);
-    const prodData = await fetchData(PROD_URL);
+  // try {
+  //   const devData = await fetchData(DEV_URL);
+  //   const testData = await fetchData(TEST_URL);
+  //   const perfData = await fetchData(PERF_URL);
+  //   const uatData = await fetchData(UAT_URL);
+  //   const preProdData = await fetchData(PPROD_URL);
+  //   const prodData = await fetchData(PROD_URL);
 
-    finalData = {
-      ...finalData,
-      Dev: devData,
-      Test: testData,
-      Perf: perfData,
-      UAT: uatData,
-      PreProd: preProdData,
-      Prod: prodData,
-    };
+  //   finalData = {
+  //     ...finalData,
+  //     Dev: devData,
+  //     Test: testData,
+  //     Perf: perfData,
+  //     UAT: uatData,
+  //     PreProd: preProdData,
+  //     Prod: prodData,
+  //   };
+  //   return finalData;
+  try {
+    const urls = [DEV_URL, TEST_URL, PERF_URL, UAT_URL, PPROD_URL, PROD_URL];
+    const keys = ["Dev", "Test", "Perf", "UAT", "PreProd", "Prod"];
+    const data = await Promise.all(urls.map((url) => fetchData(url)));
+    const finalData = {};
+    keys.forEach((key, i) => {
+      finalData[key] = data[i];
+    });
     return finalData;
   } catch (error) {
     console.log(error);
